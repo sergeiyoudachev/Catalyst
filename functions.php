1edc3bb2f9d150ba1b5374b4964690bdd6581b93
sergeiyoudachev <89488460+sergeiyoudachev@users.noreply.github.com>
2021-10-08 23:50:40 +1100

    Add files via upload
    8 Oct 2021

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php 
 
+/*------------------------------------------------------------*/
+/*  Create connection to MySQL   */
 function ConnectDB(){
 $servername = "localhost";
 $username = "root";
@@ -13,7 +15,8 @@ if ($conn->connect_error) {
 echo "Connected successfully \n";
 }
 
-//---------------------------------------------------------------------
+/*------------------------------------------------------------*/
+/*  Create MySQL DataBase  */
 
 function CreateDB(){
 $servername = "localhost";
@@ -36,7 +39,9 @@ if ($conn->query($sql) === TRUE) {
 }
 $conn->close();
 }
-//------------------------------------------------------------//
+/*------------------------------------------------------------*/
+/*  Create MySQL users table  */
+
 function CreateTable($param1,$param2,$param3){
 
 $servername = "localhost";
@@ -55,10 +60,11 @@ if ($conn->connect_error) {
 $sql = "CREATE TABLE users (
 $param1 VARCHAR(50) NOT NULL,
 $param2 VARCHAR(50) NOT NULL,
-$param3 VARCHAR(50) NOT NULL
+$param3 VARCHAR(50) NOT NULL 
 )";
 
 if ($conn->query($sql) === TRUE) {
+  $conn->query("ALTER TABLE `users` ADD UNIQUE KEY `$param3` (`$param3`)"); 
   echo "Table USERS created successfully \n";
 } else {
   echo "Error creating table: " . $conn->error;
@@ -67,22 +73,110 @@ $conn->close();
 }
 
 /*----------------------------------------------------*/
+/*    show file content and insert file content into DataBase  */
+
+
 function printFile(){
 
-   $row = 1;
+   $row = 0;
 if (($handle = fopen("users.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $num = count($data);
+        
+        if($row){
         echo " $num fields in line $row: \n";
+        }
+        // skip the header of the csv file
+         if($data[0]!='name' && !empty($data[0])){
+          insertRow($data[0], $data[1], $data[2]);
+        }
+        
         $row++;
+       
         for ($c=0; $c < $num; $c++) {
-            echo $data[$c] . "\n";
+            echo $data[$c] ." ";
         }
     }
     fclose($handle);
 }
 
 
+/*----------------------------------------------------*/
+/*  insert file content into DataBase  */
+}
+
+function  insertRow($name, $surname, $email){
+     $servername = "localhost";
+     $username = "root";
+     $password = "";
+     $dbname = "usersDB";
+    // Create connection
+    $conn = new mysqli($servername, $username, $password,$dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully \n";
+     
+
+    mysqli_query($conn, "INSERT INTO users (name, surname, email) VALUES ('$name','$surname','$email') ");
+
+}
+/*----------------------------------------------------*/
+/*   showing file content */
+
+function showFile(){
+   $row = 0;
+if (($handle = fopen("users.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        if($row){
+        echo " $num fields in line $row: \n";
+        }
+        $row++;
+        
+        for ($c=0; $c < $num; $c++) {
+             
+            echo $data[$c] ." ";
+        }
+    }
+    fclose($handle);
+}
+}
+/*----------------------------------------------------*/
+/*  otput list of directives with details */
+
+function funcHelp(){
+echo " Command line options: \n
+--file [csv file name] – show content of the CSV file to be parsed into Data Base. \n
+--insert [csv file name] - show content of the CSV file and insert content of CSV file into Data Base. \n
+--create_table – build the MySQL users table. \n 
+--dry_run (use with --file directive) run the script but not insert into the DB. All other functions will be executed, but the
+database won't be altered. Example : --dry_run users.csv \n
+-u – MySQL username.\n
+-p – MySQL password.\n
+-h – MySQL host.\n
+--help – show list of directives with details.\n";
+}
+
+/*----------------------------------------------------*/
+/* show MySQL host */
+
+function showMysqlHost(){
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+
+//$link = mysql_connect('localhost', 'root', '');
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error); 
+}
+printf("MySQL host info: %s\n", mysqli_get_host_info($conn));
 
 }
 
--- user_upload.php
+++ user_upload.php
@@ -9,25 +9,42 @@ $temp.=$argv[2];
 }
 echo $temp;
 
-if($temp=='--file users.csv'){
-	echo "call function print file \n";
+if($temp=='--insert users.csv'){
+	echo "call function print and insert file \n";
+    ConnectDB();
+    CreateDB();
+    CreateTable('name','surname','email');
     printFile();
+
+}elseif($temp=='--file users.csv'){
+	echo "call function print file \n";
+    showFile();
 
 }elseif($temp=='--create_table') {
     echo "call func create table \n"; 
     ConnectDB();
     CreateDB();
     CreateTable('name','surname','email');
-}elseif($temp=='--dry_run'){
+
+}elseif($temp=='--dry_run users.csv'){
     echo "call dry run \n";
+    ConnectDB();
+    CreateDB();
+    CreateTable('name','surname','email');
+    showFile();
+
 }elseif($temp=='-u'){
     echo "call MySQL User name \n";
+
 }elseif($temp=='-p'){
     echo "MySQL Password \n";
+
 }elseif($temp=='-h'){
     echo "MySQL host \n";
+    showMysqlHost();
+
 }elseif($temp=='--help'){
-    echo "Directories \n";
+    funcHelp();
     }else {echo "Please enter --help for instructions";}
 
 ?>
