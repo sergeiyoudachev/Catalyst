b5c01734dce05207b6d574c5a59167242125b357
sergeiyoudachev <89488460+sergeiyoudachev@users.noreply.github.com>
2021-10-09 22:43:41 +1100

    Add files via upload
    9.10.2021 10:43 PM

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php 
 
-/*------------------------------------------------------------*/
+/*------------------------------ function ConnectDB ------------------------------*/
 /*  Create connection to MySQL   */
 function ConnectDB(){
 $servername = "localhost";
@@ -15,21 +15,19 @@ if ($conn->connect_error) {
 echo "Connected successfully \n";
 }
 
-/*------------------------------------------------------------*/
+/*--------------------------function CreateDB ----------------------------------*/
 /*  Create MySQL DataBase  */
 
 function CreateDB(){
 $servername = "localhost";
 $username = "root";
 $password = "";
-
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
 // Create database
 $sql = "CREATE DATABASE usersDB";
 if ($conn->query($sql) === TRUE) {
@@ -39,7 +37,7 @@ if ($conn->query($sql) === TRUE) {
 }
 $conn->close();
 }
-/*------------------------------------------------------------*/
+/*-----------------------------function CreateTable -------------------------------*/
 /*  Create MySQL users table  */
 
 function CreateTable($param1,$param2,$param3){
@@ -55,25 +53,23 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
 // sql to create table
 $sql = "CREATE TABLE users (
 $param1 VARCHAR(50) NOT NULL,
 $param2 VARCHAR(50) NOT NULL,
 $param3 VARCHAR(50) NOT NULL 
 )";
-
 if ($conn->query($sql) === TRUE) {
-  $conn->query("ALTER TABLE `users` ADD UNIQUE KEY `$param3` (`$param3`)"); 
-  echo "Table USERS created successfully \n";
+   $conn->query("ALTER TABLE `users` ADD UNIQUE KEY `$param3` (`$param3`)"); 
+   echo "Table USERS created successfully \n";
 } else {
   echo "Error creating table: " . $conn->error;
 }
 $conn->close();
 }
 
-/*----------------------------------------------------*/
-/*    show file content and insert file content into DataBase  */
+/*-----------------------function printFile -----------------------------*/
+/*    show, checking and insert file content into DataBase  */
 
 
 function printFile(){
@@ -82,16 +78,20 @@ function printFile(){
 if (($handle = fopen("users.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $num = count($data);
-        
         if($row){
-        echo " $num fields in line $row: \n";
+        echo "  \n";
         }
         // skip the header of the csv file
          if($data[0]!='name' && !empty($data[0])){
+        // make First character is UPPER case
+         $data[0] = ucfirst(strtolower($data[0])); 
+         $data[1] = ucfirst(strtolower($data[1])); 
+        // validate email
+          validateEmail($data[2]);
+        // insert data into DB
           insertRow($data[0], $data[1], $data[2]);
         }
-        
-        $row++;
+         $row++;
        
         for ($c=0; $c < $num; $c++) {
             echo $data[$c] ." ";
@@ -101,7 +101,7 @@ if (($handle = fopen("users.csv", "r")) !== FALSE) {
 }
 
 
-/*----------------------------------------------------*/
+/*----------------------function insertRow ------------------------------*/
 /*  insert file content into DataBase  */
 }
 
@@ -116,34 +116,33 @@ function  insertRow($name, $surname, $email){
     if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
     }
-    echo "Connected successfully \n";
-     
-
+    
+    $name=$conn->real_escape_string($name);
+    $surname=$conn->real_escape_string($surname);
+    $email=$conn->real_escape_string($email);
     mysqli_query($conn, "INSERT INTO users (name, surname, email) VALUES ('$name','$surname','$email') ");
-
 }
-/*----------------------------------------------------*/
+/*------------------------function showFile ----------------------------*/
 /*   showing file content */
 
 function showFile(){
+ echo "USERS.CSV \n\n";
    $row = 0;
 if (($handle = fopen("users.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $num = count($data);
         if($row){
-        echo " $num fields in line $row: \n";
+        echo "  \n";
         }
         $row++;
-        
         for ($c=0; $c < $num; $c++) {
-             
             echo $data[$c] ." ";
         }
     }
     fclose($handle);
 }
 }
-/*----------------------------------------------------*/
+/*------------------------- function funcHelp ---------------------------*/
 /*  otput list of directives with details */
 
 function funcHelp(){
@@ -159,25 +158,52 @@ database won't be altered. Example : --dry_run users.csv \n
 --help – show list of directives with details.\n";
 }
 
-/*----------------------------------------------------*/
+/*----------------------- function showMysqlHost -----------------------------*/
 /* show MySQL host */
 
 function showMysqlHost(){
-
 
 $servername = "localhost";
 $username = "root";
 $password = "";
 // Create connection
 $conn = new mysqli($servername, $username, $password);
-
-
-//$link = mysql_connect('localhost', 'root', '');
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error); 
 }
 printf("MySQL host info: %s\n", mysqli_get_host_info($conn));
+}
 
+
+/*------------------------- function validateEmail ---------------------------*/
+/* function receive EMAIL Address and return changed EMAIL Address or message if EMAIL doesnt valid.
+validate EMAIL Address and set to be lover case before insert to DB. In case if EMAIL doesnt valid - replace 
+incorrect email address to message "Invalid Email address" */
+    
+function validateEmail($email){
+
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $email = strtolower($email);
+               } else {
+                $email="Invalid EMAIL";
+          echo "\n" ."WARNING! -> Email address " .$email ." is considered invalid.\n";
+          }
+          return $email;
+}
+
+/*----------------------- function userMySql -----------------------------*/
+/* show MySQL username */
+
+function userMySql(){
+
+echo "MySQL username - root \n";
+}
+/*----------------------- function passwordMySql -----------------------------*/
+/* show MySQL password */
+
+function passwordMySql(){
+
+echo "MySQL password ia blank \n";
 }
 
 ?>
--- user_upload.php
+++ user_upload.php
@@ -1,50 +1,58 @@
 <?php include "functions.php";?>
 <?php
 
-$tempc=$argc;
+if($argc == 2){  
+
 $temp=$argv[1];
-if($argc>2){
+
+}elseif($argc==3){
+$temp=$argv[1];
 $temp .=" "; 
-$temp.=$argv[2];
+$temp .=$argv[2];
+}else{
+$temp ='--help';
 }
-echo $temp;
 
 if($temp=='--insert users.csv'){
-	echo "call function print and insert file \n";
+	
     ConnectDB();
     CreateDB();
     CreateTable('name','surname','email');
     printFile();
 
 }elseif($temp=='--file users.csv'){
-	echo "call function print file \n";
+	
     showFile();
 
 }elseif($temp=='--create_table') {
-    echo "call func create table \n"; 
+     
     ConnectDB();
     CreateDB();
     CreateTable('name','surname','email');
 
 }elseif($temp=='--dry_run users.csv'){
-    echo "call dry run \n";
+    
     ConnectDB();
     CreateDB();
     CreateTable('name','surname','email');
     showFile();
 
 }elseif($temp=='-u'){
-    echo "call MySQL User name \n";
+    
+    userMySql();
 
 }elseif($temp=='-p'){
-    echo "MySQL Password \n";
+
+    passwordMySql();
 
 }elseif($temp=='-h'){
-    echo "MySQL host \n";
+ 
     showMysqlHost();
 
 }elseif($temp=='--help'){
+
     funcHelp();
+
     }else {echo "Please enter --help for instructions";}
 
 ?>
